800efcdac7fdb8cf9638b790fde06d02ec7912ac
Chris <167183247+demodrome@users.noreply.github.com>
2024-09-08 05:57:33 +0100

    docs: Add documentation for the game module

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,19 @@
 import Player from "./player.js";
 import gameBoard from "./gameboard.js";
 
+/**
+ * The game module contains the primary logic for playing the game. It implements the
+ * game loop and checking for win conditions.
+ * @module
+ * @returns {Object} The game API
+ */
 const game = (function() {
   const playerOne = new Player('Player 1', 'X');
   const playerTwo = new Player('Player 2', 'O');
 
+  /**
+   * The game loop logic controlling how a round is played.
+   */
   function playRound() {
     let hasWinner = false;
     let activePlayer = playerOne;
@@ -41,4 +50,5 @@ const game = (function() {
   }
 })();
 
+// Start the game loop
 game.playRound();
