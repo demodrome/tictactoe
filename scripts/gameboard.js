8df186a50ca28387c76e488e8f26e072aa4e5f3e
Chris <167183247+demodrome@users.noreply.github.com>
2024-09-08 05:52:39 +0100

    feat: Add initial game loop

--- scripts/gameboard.js
+++ scripts/gameboard.js
@@ -54,6 +54,9 @@ const gameBoard = (function() {
     console.log(`${board[0][0]},${board[0][1]},${board[0][2]}`);
     console.log(`${board[1][0]},${board[1][1]},${board[1][2]}`);
     console.log(`${board[2][0]},${board[2][1]},${board[2][2]}`);
+    console.log('------------------------------------------------');
+
+    return board;
   }
 
   return {
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,44 @@
-console.log('Script linked');
+import Player from "./player.js";
+import gameBoard from "./gameboard.js";
+
+const game = (function() {
+  const playerOne = new Player('Player 1', 'X');
+  const playerTwo = new Player('Player 2', 'O');
+
+  function playRound() {
+    let hasWinner = false;
+    let activePlayer = playerOne;
+    gameBoard.reset();
+
+    let i = 0;
+
+    while (!hasWinner) {
+      console.log('Game started');
+      const x = prompt(`${activePlayer.getName()}'s turn: X ->`);
+      const y = prompt(`${activePlayer.getName()}'s turn: Y ->`);
+
+      gameBoard.mark({ x: x, y: y, marker: activePlayer.getMarker() });
+
+      gameBoard.getBoard();
+
+      i++;
+
+      if (i >= 5) {
+        hasWinner = true;
+      }
+      
+      // Switch players
+      if (activePlayer === playerOne) {
+        activePlayer = playerTwo;
+      } else {
+        activePlayer = playerOne;
+      }
+    }
+  }
+
+  return {
+    playRound
+  }
+})();
+
+game.playRound();
